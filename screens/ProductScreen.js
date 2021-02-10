69e2951e2c7c648e294c60da46e82b97e4dbf787
Obinna Mokwe <obinnamokwe@Obinnas-MacBook-Air.local>
2021-02-10 00:36:52 -0600

    first commit

---
+++ screens/ProductScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
+
+const ProductScreen = () => {
+    return (
+       <Card className={classes.root}>
+           <CardMedia className={classes.media} image='' />
+
+       </Card>
+    )
+}
+
+export default ProductScreen
+
+
